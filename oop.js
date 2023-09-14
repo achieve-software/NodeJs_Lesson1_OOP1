e8356e50ed1e762c67f2a50a4acae9a64c135064
Achieve-software <vtcbcdtyr@gmail.com>
2023-09-14 18:01:17 +0300

    constr

--- oop.js
+++ oop.js
@@ -103,3 +103,94 @@ const testArray = [ 'value0', 'value1', 'value2', 'value3' ]
 const newArr = [ ...testArray, 'new-value', 'new-value2' ]
 console.log( newArr )
 */
+
+
+/*
+//? OBJECT DESTRUCTURING
+
+const Car = {
+
+    brand: 'Ford',
+    model: 'Mustang',
+    year: 1967,
+    isAutoGear: true,
+    colors: ['white', 'red'],
+    details: {
+        color1: 'red',
+        color2: 'white',
+        engineSize: 4900
+    },
+    startEngine: function () {
+        return 'Motor çalıştı'
+    }
+}
+
+// Rest:
+// const { year, model, brand, ...otherItems } = Car
+// console.log( year, model, brand )
+// console.log( otherItems )
+
+const { year: modelYear, model: newName, brand } = Car
+console.log( modelYear, newName, brand )
+console.log ( Car ) // Orjinal değişmiyor.
+
+// Spread:
+const newObj = {
+    ...Car.colors,
+    newKey: 'new-value'
+}
+console.log(newObj)
+
+// Object to JSON:
+const json = JSON.stringify(Car)
+console.log ( typeof json, json )
+
+// JSON to Object:
+const newObj2 = JSON.parse( json )
+console.log(typeof newObj2, newObj2)
+
+// Object to Array:
+// const arr = [ ...Car ]
+const arr = Object.entries(Car)
+console.log( arr )
+const arr2 = Object.values(Car)
+console.log(arr2)
+const arr3 = Object.keys(Car)
+console.log(arr3
+
+    */
+
+    /* -------------------------------------------------------
+    Object Constructor
+------------------------------------------------------- */
+
+const PascalCaseNamed = function () {
+    this.property = 'value'
+}
+
+/* ------------------------------------------------------- */
+//? "NEW" KEYWORD
+
+const CarConstructor = function (brand, model, year = 2011) {
+
+    this.brand = brand
+    this.model = model
+    this.year = year
+    this.isRunning = false
+    this.startEngine = function () {
+        this.isRunning = true
+        return 'Motor çalıştı'
+    }
+}
+
+const newCar = new CarConstructor('Ford', 'Mustang', 1967)
+console.log( newCar )
+
+const newCar2 = new CarConstructor('Toyota', 'Corolla')
+console.log( newCar2 )
+
+console.log( newCar2.isRunning )
+console.log( newCar2.startEngine() )
+console.log( newCar2.isRunning )
+
+/* ------------------------------------------------------- */
