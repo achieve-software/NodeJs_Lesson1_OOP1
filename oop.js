ca912464016dc2ce31771f5390d538f4f3f1ae12
Achieve-software <vtcbcdtyr@gmail.com>
2023-09-14 16:39:51 +0300

    car obj

---
+++ oop.js
@@ -0,0 +1,48 @@
+"use strict"
+// --------------------------------------------
+// OBJECTS
+
+
+// const exampleObject = {
+
+//     propertyName: 'value', // field, attribute
+//     methodName: function () {
+//         return 'This is Method'
+//     }
+// }
+
+// console.log( exampleObject.propertyName )
+// console.log( exampleObject.methodName() )
+
+
+
+/* 
+const Car = {
+
+    brand: 'Ford',
+    model: 'Mustang',
+    year: 1967,
+    isAutoGear: true,
+    colors: ['white', 'red'],
+    details: {
+        color1: 'red',
+        color2: 'white',
+        engineSize: 4900
+    },
+    startEngine: function () {
+        return 'Motor çalıştı'
+    }
+}
+
+// console.log( Car.brand )
+// console.log( Car.colors )
+// console.log( Car.colors[0] )
+// console.log( Car.details )
+// console.log( Car.details.engineSize )
+// console.log( Car.startEngine() )
+
+console.log( Car['brand'] )
+console.log( Car.details['engineSize'] )
+console.log( Car['details']['engineSize'] )
+console.log( Car['startEngine']() )
+*/
